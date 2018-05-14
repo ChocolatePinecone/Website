ff3778fda091bd986497c393f04d51c10da1019b
jelmer <jelmer.pijnappel@ing.nl>
2018-05-14 14:17:02 +0200

    Added games page skeleton

--- js/browser-check.js
+++ js/browser-check.js
@@ -1,6 +1,7 @@
 // Internet Explorer 6-11
 let isIE = !!document.documentMode;
 
+// If site is opened with Internet Explorer, change to "browser-unsupported" page
 if(isIE === true) {
     window.location.href = 'https://chocolatepinecone.github.io/Website/browser-unsupported.html';
 }
