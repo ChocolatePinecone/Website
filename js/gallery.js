f1cd82030e68ef8ece6965946896ddbb46b35198
jelmer <jelmer.pijnappel@ing.nl>
2018-06-05 09:00:41 +0200

    Added title to game page and added function to show gameplay in gallery

--- js/gallery.js
+++ js/gallery.js
@@ -6,18 +6,23 @@
  */
 
 // Functions
-let setGalleryHeight = () => {
+let calibrateGalleryHeight = () => {
     // Find highest gallery item and set gallery height\
     let height = 0;
     for(item of galleryItems) {
         height = (item.querySelector('img').height > height) ? item.querySelector('img').height : height;
     }
-    console.log('height set to: ', height);
+    gallery.style.height = '' + height + 'px';
+};
+
+let setGalleryHeightToGif = () => {
+    // Find height of gif and set gallery height\
+    let height = gif.querySelector('img').height;
     gallery.style.height = '' + height + 'px';
 };
 
 let imageNr = 0;
-let fade = () => {
+let fadeToNext = () => {
     window.setInterval(() => {
         // Save currently active item nr
         let imageNrToDeactivate = imageNr;
@@ -33,8 +38,34 @@ let fade = () => {
     }, 3000);
 };
 
+let fadeTextInOut = () => {
+    window.setInterval(() => {
+        // Fade text in and out
+        if(text.classList.contains('active')) {
+            text.classList.remove('active');
+        }
+        else {
+            text.classList.add('active');
+        }
+        galleryItems[imageNr].classList.add('active');
+    }, 1000);
+};
+
+let toggleGif = () => {
+    if(gif.classList.contains('show') === false) {
+        setGalleryHeightToGif();
+        gif.classList.add('show');
+    }
+    else {
+        calibrateGalleryHeight();
+        gif.classList.remove('show');
+    }
+};
+
 // Find gallery items
 let gallery = document.getElementsByClassName('gallery')[0];
+let text = gallery.getElementsByClassName('text')[0];
+let gif = gallery.getElementsByClassName('hover')[0];
 let galleryItems = gallery.getElementsByClassName('item');
 
 // Set up first gallery item
@@ -42,10 +73,16 @@ galleryItems[0].classList.add('active');
 
 // Start fading animation cycle
 window.onload = () => {
-    setGalleryHeight();
-    fade();
+    calibrateGalleryHeight();
+    fadeToNext();
+
+    if(text !== undefined) {
+        fadeTextInOut();
+
+        gallery.addEventListener('click', toggleGif);
+    }
 };
 
 window.onresize = () => {
-    setGalleryHeight();
+    calibrateGalleryHeight();
 };
